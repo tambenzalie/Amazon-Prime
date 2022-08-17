38c051e7e41f658bf70ffd43b7823a98d31dc24a
Howard <howie8183@hotmail.com>
2022-08-18 00:49:29 +0100

    added cloud front code for caching @task23456

---
+++ cloudfront.tf
@@ -0,0 +1,27 @@
+#Cloudfront is used to cache the movies data
+resource "aws_s3_bucket" "b" {
+  bucket = "mybucket"
+
+  tags = {
+    Name = "My bucket"
+  }
+}
+
+resource "aws_s3_bucket_acl" "b_acl" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
+}
+
+locals {
+  s3_origin_id = "myS3Origin"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.b.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+
+    s3_origin_config {
+      origin_access_identity = "origin-access-identity/cloudfront/ABCDEFG1234567"
+    }
+  }
